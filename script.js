d20aa0a7feba09a7962ee3382cd985b14b099940
Jogi <stigmahotspot@gmail.com>
2022-11-22 16:14:10 +0700

    finishing section b dan section c

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-Swal.fire({
-    icon: 'success',
-    title: 'Oops...',
-    text: 'Something went wrong!',
+function contoh(){
+  Swal.fire({
+    title: 'Kamu yakin dengan pesanan mu?',
+    text: "",
+    icon: 'warning',
+    showCancelButton: true,
+    cancelButtonColor: '#d33',
+    confirmButtonColor: '#379237',
+    cancelButtonText: 'Nanti deh',
+    confirmButtonText: 'Pastinya'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire(
+        'Pesanan telah diterima',
+        'Pesanan anda akan segera dikerjakan',
+        'success'
+      )
+    }
   })
+}
